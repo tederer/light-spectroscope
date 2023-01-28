e9bc7277e096bff61e7e5cb87e7eae06c25e3a92
Thomas Ederer <2210781006@fh-burgenland.at>
2023-01-28 18:16:01 +0100

    implemented clipboard button in table view

--- src/client/TableViewTab.js
+++ src/client/TableViewTab.js
@@ -1,40 +1,57 @@
-/* global spectroscope, assertNamespace, common */
+/* global spectroscope, assertNamespace, common, navigator, setTimeout */
 
 assertNamespace('spectroscope.client');
 
 spectroscope.client.TableViewTab = function TableViewTab(bus) {
 
-   const CSS_SELECTOR      = '#tableViewTab #values';
-   const RAW_PREFIX        = 'raw_';
-   const CALIBRATED_PREFIX = 'calibrated_';
+   const VALUES_CSS_SELECTOR = '#tableViewTab #values';
+   const UNIT_CSS_SELECTOR   = '#tableViewTab #unit';
+   const RAW_PREFIX          = 'raw_';
+   const CALIBRATED_PREFIX   = 'calibrated_';
+   const COLOR_PREFIX        = 'color_';
+   const OK                  = 'okState';
+   const ERROR               = 'errorState';
 
    var connected        = false;
    var tableInitialized = false;
+   var unitInitialized  = false;
    var sensorValues;
    var waveLengthNames;
+   var copyToClipboardTask;
+
+   var setUnit = function setUnit(unit) {
+      $(UNIT_CSS_SELECTOR).text(' (in ' + unit + ')');
+   };
+
+   var removeUnit = function removeUnit() {
+      $(UNIT_CSS_SELECTOR).text('');
+   };
 
    var createTable = function createTable(waveLengthNames) {
-      var content = '<table class="table table-dark table-striped"><thead><th>wave length</th><th>calibrated</th><th>raw</th></thead><tbody>';
+      var content = '<table class="table table-dark table-striped"><thead><th></th><th>wave length</th><th>calibrated</th><th>raw</th></thead><tbody>';
       waveLengthNames.forEach(name => {
          var formattedRowName = name.replace(/(\d+)/, '$1 ');
          content +=  '<tr class="table-light">' +
+                     '<td><div class="' + COLOR_PREFIX + name   + '">&nbsp;</div></td>' + 
                      '<td>'     + formattedRowName         + '</td>' + 
                      '<td id="' + CALIBRATED_PREFIX + name + '"></td>' +
                      '<td id="' + RAW_PREFIX + name        + '"></td></tr>';
       });
       content += '</tbody></table>';
-      $(CSS_SELECTOR).html(content);
+      $(VALUES_CSS_SELECTOR).html(content);
    };
 
    var removeTable = function removeTable() {
-      $(CSS_SELECTOR).html('');
+      $(VALUES_CSS_SELECTOR).html('');
    };
 
    var updateUi = function updateUi() {
       if (!connected) {
          removeTable();
+         removeUnit();
          waveLengthNames  = undefined;
          tableInitialized = false;
+         unitInitialized  = false;
          return;
       }
       
@@ -45,10 +62,15 @@ spectroscope.client.TableViewTab = function TableViewTab(bus) {
             createTable(waveLengthNames);
          }
 
+         if (!unitInitialized) {
+            unitInitialized = true;
+            setUnit(sensorValues.calibratedValues.unit);
+         }
+         
          [{values: 'calibratedValues', prefix: CALIBRATED_PREFIX}, {values: 'rawValues', prefix: RAW_PREFIX}].forEach(tableColumn => {
             waveLengthNames.forEach(waveLengthName => {
                var value = sensorValues[tableColumn.values].values[waveLengthName];
-               $(CSS_SELECTOR + ' #' + tableColumn.prefix + waveLengthName).text(value);
+               $(VALUES_CSS_SELECTOR + ' #' + tableColumn.prefix + waveLengthName).text(value);
             });   
          });
       }
@@ -80,6 +102,38 @@ spectroscope.client.TableViewTab = function TableViewTab(bus) {
       }
    };
 
+   var highlightClipboardIcon = function highlightClipboardIcon(cssClassToUse) {
+      $('#copyToClipboardIcon').addClass(cssClassToUse);
+      copyToClipboardTask = setTimeout(() => {
+         $('#copyToClipboardIcon').removeClass(cssClassToUse);
+         copyToClipboardTask = undefined;
+      }, 1000);
+   };
+
+
+   var copyDataToClipboard = function copyDataToClipboard() {
+      if ((copyToClipboardTask === undefined) && (waveLengthNames !== undefined) && (sensorValues !== undefined)) {
+         var text    = (new Date()).toISOString() + '\ncalibrated values:\n';
+         var heading = '';
+         waveLengthNames.forEach(waveLengthName => {
+            heading += waveLengthName + ';';
+         });
+         text += heading + '\n';
+         waveLengthNames.forEach(waveLengthName => {
+            text += sensorValues.calibratedValues.values[waveLengthName] + ';';
+         });
+         text += '\nraw values:\n' + heading + '\n';
+         waveLengthNames.forEach((waveLengthName, index) => {
+            text += sensorValues.rawValues.values[waveLengthName] + ';';
+         });
+         navigator.clipboard.writeText(text)
+            .then(()  => highlightClipboardIcon(OK))
+            .catch(() => highlightClipboardIcon(ERROR));
+      }
+   };
+
    bus.subscribeToPublication(spectroscope.client.topics.CONNECTED, onConnected);
    bus.subscribeToPublication(spectroscope.shared.topics.SENSOR_VALUES, onSensorValuesReceived);
+
+   $('#copyToClipboardIcon').click(copyDataToClipboard);
 };
